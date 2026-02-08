e88a838655bcac18ddd832060271eb2952e5ab29
Somnus <chen1921460502@outlook.com>
2026-02-08 11:31:53 +0800

    feat: Add "Sudoku Solver" kata, update statistics and index

---
+++ katas/6kyu/006_are_they_the_550498447451fbbd7600041c/src/lib.rs
@@ -0,0 +1,58 @@
+//! Title: Are they the \
+//! Link: https://www.codewars.com/kata/550498447451fbbd7600041c
+//! Kata ID: 550498447451fbbd7600041c
+//! Rank: 6kyu
+//! Completed: 2026-02-08
+#![allow(dead_code)]
+
+fn comp(a: Vec<i64>, b: Vec<i64>) -> bool {
+    // your code
+    if a.len() != b.len() {
+        return false;
+    }
+    if a.is_empty() || b.is_empty() {
+        return false;
+    }
+    let mut a_squared: Vec<i64> = a.iter().map(|&x| x * x).collect();
+    a_squared.sort();
+    let mut b_sorted = b.clone();
+    b_sorted.sort();
+    a_squared == b_sorted
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn testing(a: Vec<i64>, b: Vec<i64>, exp: bool) -> () {
+        assert_eq!(comp(a, b), exp)
+    }
+
+    #[test]
+    fn tests_comp() {
+        let a1 = vec![121, 144, 19, 161, 19, 144, 19, 11];
+        let a2 = vec![
+            11 * 11,
+            121 * 121,
+            144 * 144,
+            19 * 19,
+            161 * 161,
+            19 * 19,
+            144 * 144,
+            19 * 19,
+        ];
+        testing(a1, a2, true);
+        let a1 = vec![121, 144, 19, 161, 19, 144, 19, 11];
+        let a2 = vec![
+            11 * 21,
+            121 * 121,
+            144 * 144,
+            19 * 19,
+            161 * 161,
+            19 * 19,
+            144 * 144,
+            19 * 19,
+        ];
+        testing(a1, a2, false);
+    }
+}
